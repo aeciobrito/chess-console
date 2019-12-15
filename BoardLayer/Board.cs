577ca71ff28344f3cfae874cdb0d7ad527ede7b9
Aecio Desktop <aeciobrito@gmail.com>
2019-12-15 11:12:50 -0300

    Positions logic and excepetion to invalid or busy tile

--- BoardLayer/Board.cs
+++ BoardLayer/Board.cs
@@ -23,12 +23,25 @@ namespace BoardLayer
             return _tiles[position.x, position.y];
         }
 
+        public bool BusyPlace(Vector2 position)
+        {
+            ValidPosition(position);
+            return Piece(position) != null;
+        }
+
         public void SetPiece(Piece piece, Vector2 position)
         {
+            if (BusyPlace(position))
+                throw new BoardExeption("There is a piece here already");
+
             _tiles[position.x, position.y] = piece;
             piece.SetPosition(position);
         }
 
-
+        public void ValidPosition(Vector2 position)
+        {
+            if (position.x < 0 || position.x > Size.x || position.y < 0 || position.y > Size.y)
+                throw new BoardExeption("Invalid Position");
+        }
     }
 }
---
+++ BoardLayer/BoardExeption.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BoardLayer
+{
+    class BoardExeption : Exception
+    {
+        public BoardExeption(string message) : base(message)
+        {
+
+        }
+    }
+}
